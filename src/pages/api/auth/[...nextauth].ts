0dfe0ffeb951ff54030b52ca9e67729c71ec30b8
cezarcozta <cezarcozta@gmail.com>
2022-06-19 15:49:25 -0300

    remove error var that are not used

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,19 @@
-import { query as q } from 'faunadb';
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-import { fauna } from '../../../services/fauna';
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // providers configuration
   providers: [
-   Providers.GitHub({
+    Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
   callbacks: {
-    async signIn(user, account, profile){
+    async signIn(user, account, profile) {
       const { email } = user;
 
       try {
@@ -21,29 +21,18 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
         );
-        
+
         return true;
-      } catch (error) {
+      } catch {
         return false;
       }
-    }
-  }
-})
+    },
+  },
+});
