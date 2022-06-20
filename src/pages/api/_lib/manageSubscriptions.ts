b36624ce346594d316200e031fafdf14951f9e24
cezarcozta <cezarcozta@gmail.com>
2022-06-19 23:41:10 -0300

    listen only checkout session

--- src/pages/api/_lib/manageSubscriptions.ts
+++ src/pages/api/_lib/manageSubscriptions.ts
@@ -1,7 +1,11 @@
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
-export async function saveSubscription(subscriptionId, customerId) {
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  creationEvent = false
+) {
   const userRef = await fauna.query(
     q.Select(
       "ref",
@@ -18,7 +22,19 @@ export async function saveSubscription(subscriptionId, customerId) {
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create(q.Collection("subscriptions"), { data: subscriptionData })
-  );
+  if (creationEvent) {
+    await fauna.query(
+      q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscription_by_id"), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
